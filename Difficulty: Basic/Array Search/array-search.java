936066865ca6baebd5c5da4f68adaecd34c1d8b5
Pratyush Kumar Pandey <2300030557@kluniversity.in>
2025-07-06 17:56:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Search/array-search.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+    public int search(int[] arr, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int x = sc.nextInt();
+        Solution obj = new Solution();
+        System.out.println(obj.search(nums, x));
+        sc.close();
+    }
+}
